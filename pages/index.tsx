f903c285236426f0cfb2e46885ccacf0ff6dacd1
GerBear21 <138827342+GerBear21@users.noreply.github.com>
2026-04-10 06:21:16 +0200

    fixing the frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,7 @@ export default function Home() {
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
               </span>
-              <span className="text-gray-600 text-xs font-semibold tracking-wide uppercase">System Operational</span>
+             
             </motion.div>
 
             <motion.h1
